5d1e77428a2975ffa0af31f61884f9a9d331a126
JaxXu <yihaoxu1998@gmail.com>
2016-08-04 23:13:24 +0800

    SWF解析类

---
+++ src/main/java/com/jax/seer2tools/util/SWFSub.java
@@ -0,0 +1,124 @@
+package com.jax.seer2tools.util;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.RandomAccessFile;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.zip.Inflater;
+
+public class SWFSub {
+	public SWFSub() throws Exception {
+		download();
+	}
+
+	public void download() throws Exception {
+		int bytesum = 0;
+		int byteread = 0;
+		URL url = new URL("http://seer2.61.com/dll/ClientAppDLL.swf");
+		URLConnection conn = url.openConnection();
+		InputStream inStream = conn.getInputStream();
+		FileOutputStream fs = new FileOutputStream("src/main/resources/util/ClientAppDLL.swf");
+		byte[] buffer = new byte[1024];
+		int length;
+		while ((byteread = inStream.read(buffer)) != -1) {
+			bytesum += byteread;
+			System.out.println(bytesum);
+			fs.write(buffer, 0, byteread);
+		}
+		fs.close();
+		inStream.close();
+
+		// 取出前7字节
+		RandomAccessFile raFile = new RandomAccessFile("src/main/resources/util/ClientAppDLL.swf", "rw");
+		raFile.seek(7); // 定位到第8字节
+		byte[] data = new byte[(int) raFile.length() - 7];
+		raFile.read(data);
+		// 解压缩
+		byte[] finalByte = decompress(data);
+		File file = new File("src/main/resources/util/ClientAppDLL.swf");
+		file.deleteOnExit();
+
+		// 输出压缩后的文件
+		OutputStream fos = new FileOutputStream(new File("src/main/resources/util/new.swf"));
+		fos.write(finalByte);
+		fos.flush();
+		fos.close();
+
+		// 解析swf输出xml文件
+		Runtime.getRuntime()
+				.exec("cmd /c cd /d src/main/resources/util && java -jar ffdec.jar -export binaryData ../xml new.swf");
+
+		// 去除不规范的xml文件前缀
+		while (true) {
+			File newfile = new File(
+					"src/main/resources/xml/77_com.taomee.seer2.app.config.PetConfig__dictionaryXmlClass_com.taomee.seer2.app.config.PetConfig__dictionaryXmlClass.bin");
+			if (newfile.exists()) {
+				BufferedReader bre = null;
+				OutputStreamWriter pw = null;
+				bre = new BufferedReader(new InputStreamReader(
+						new FileInputStream(
+								"src/main/resources/xml/77_com.taomee.seer2.app.config.PetConfig__dictionaryXmlClass_com.taomee.seer2.app.config.PetConfig__dictionaryXmlClass.bin"),
+						"UTF-8"));
+				pw = new OutputStreamWriter(
+						new FileOutputStream(
+								"src/main/resources/xml/new77_com.taomee.seer2.app.config.PetConfig__dictionaryXmlClass_com.taomee.seer2.app.config.PetConfig__dictionaryXmlClass.bin"),
+						"UTF-8");
+				String str = "";
+				boolean flags = false;
+				while ((str = bre.readLine()) != null) {
+					if (str.indexOf("version") > 0) {
+						flags = true;
+					}
+					if (flags) {
+						pw.write(str);
+						System.out.println(str);
+					}
+				}
+				bre.close();
+				pw.close();
+				break;
+			}
+		}
+		parseXML pa = new parseXML();
+	}
+
+	public static byte[] decompress(byte[] data) {
+		byte[] output = new byte[0];
+
+		Inflater decompresser = new Inflater();
+		decompresser.reset();
+		decompresser.setInput(data);
+
+		ByteArrayOutputStream o = new ByteArrayOutputStream(data.length);
+		try {
+			byte[] buf = new byte[1024];
+			while (!decompresser.finished()) {
+				int i = decompresser.inflate(buf);
+				o.write(buf, 0, i);
+			}
+			output = o.toByteArray();
+		} catch (Exception e) {
+			output = data;
+			e.printStackTrace();
+		} finally {
+			try {
+				o.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		decompresser.end();
+		return output;
+	}
+}
