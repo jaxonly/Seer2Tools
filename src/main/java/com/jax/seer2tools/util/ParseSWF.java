f6bc1fbe1173e7e3b74d13bdb7a6d643f06869e5
JaxXu <yihaoxu1998@gmail.com>
2016-08-05 22:39:46 +0800

    更改Util类类名
    对ParseSWF、ParseXML类进行完善
    
    fix ParseSWF、ParseXML
    
    去掉target的版本控制
    
    	renamed:    src/main/java/com/jax/seer2tools/util/SWFSub.java -> src/main/java/com/jax/seer2tools/util/ParseSWF.java
    	new file:   src/main/java/com/jax/seer2tools/util/ParseXML.java
    	deleted:    src/main/java/com/jax/seer2tools/util/parseXML.java
    	deleted:    target/classes/jdbc.properties
    	deleted:    target/classes/log4j.properties
    	deleted:    target/classes/spring-mvc.xml
    	deleted:    target/classes/spring-mybatis.xml
    	deleted:    target/m2e-wtp/web-resources/META-INF/MANIFEST.MF
    	deleted:    target/m2e-wtp/web-resources/META-INF/maven/com.jax.seer2tools/seer2tools/pom.properties
    	deleted:    target/m2e-wtp/web-resources/META-INF/maven/com.jax.seer2tools/seer2tools/pom.xml

--- src/main/java/com/jax/seer2tools/util/ParseSWF.java
+++ src/main/java/com/jax/seer2tools/util/ParseSWF.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
@@ -12,52 +13,28 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.RandomAccessFile;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.zip.Inflater;
 
-public class SWFSub {
-	public SWFSub() throws Exception {
-		download();
-	}
+public class ParseSWF {
 
-	public void download() throws Exception {
-		int bytesum = 0;
-		int byteread = 0;
-		URL url = new URL("http://seer2.61.com/dll/ClientAppDLL.swf");
-		URLConnection conn = url.openConnection();
-		InputStream inStream = conn.getInputStream();
-		FileOutputStream fs = new FileOutputStream("src/main/resources/util/ClientAppDLL.swf");
-		byte[] buffer = new byte[1024];
-		int length;
-		while ((byteread = inStream.read(buffer)) != -1) {
-			bytesum += byteread;
-			System.out.println(bytesum);
-			fs.write(buffer, 0, byteread);
-		}
-		fs.close();
-		inStream.close();
+	public ParseSWF() throws Exception {
+		downloadFile();
 
-		// 取出前7字节
-		RandomAccessFile raFile = new RandomAccessFile("src/main/resources/util/ClientAppDLL.swf", "rw");
-		raFile.seek(7); // 定位到第8字节
-		byte[] data = new byte[(int) raFile.length() - 7];
-		raFile.read(data);
-		// 解压缩
+		byte[] data = get7Byte();
+		// 解压
 		byte[] finalByte = decompress(data);
-		File file = new File("src/main/resources/util/ClientAppDLL.swf");
-		file.deleteOnExit();
 
-		// 输出压缩后的文件
-		OutputStream fos = new FileOutputStream(new File("src/main/resources/util/new.swf"));
-		fos.write(finalByte);
-		fos.flush();
-		fos.close();
+		outPutFile(finalByte);
 
-		// 解析swf输出xml文件
-		Runtime.getRuntime()
-				.exec("cmd /c cd /d src/main/resources/util && java -jar ffdec.jar -export binaryData ../xml new.swf");
+		parseSWFtoXML();
 
+		deleteNoGuiFanXML();
+	}
+	private void deleteNoGuiFanXML() throws UnsupportedEncodingException, FileNotFoundException, IOException {
 		// 去除不规范的xml文件前缀
 		while (true) {
 			File newfile = new File(
@@ -81,7 +58,6 @@ public class SWFSub {
 					}
 					if (flags) {
 						pw.write(str);
-						System.out.println(str);
 					}
 				}
 				bre.close();
@@ -89,7 +65,43 @@ public class SWFSub {
 				break;
 			}
 		}
-		parseXML pa = new parseXML();
+	}
+	private void parseSWFtoXML() throws IOException {
+		// 解析swf输出xml文件
+		Runtime.getRuntime()
+				.exec("cmd /c cd /d src/main/resources/util && java -jar ffdec.jar -export binaryData ../xml new.swf");
+	}
+	private void outPutFile(byte[] finalByte) throws FileNotFoundException, IOException {
+		// 输出压缩后的文件
+		OutputStream fos = new FileOutputStream(new File("src/main/resources/util/new.swf"));
+		fos.write(finalByte);
+		fos.flush();
+		fos.close();
+	}
+	private byte[] get7Byte() throws FileNotFoundException, IOException {
+		// 取出前7字节
+		RandomAccessFile raFile = new RandomAccessFile("src/main/resources/util/ClientAppDLL.swf", "rw");
+		raFile.seek(7); // 定位到第8字节
+		byte[] data = new byte[(int) raFile.length() - 7];
+		raFile.read(data);
+		return data;
+	}
+	private void downloadFile() throws MalformedURLException, IOException, FileNotFoundException {
+		int bytesum = 0;
+		int byteread = 0;
+		URL url = new URL("http://seer2.61.com/dll/ClientAppDLL.swf");
+		URLConnection conn = url.openConnection();
+		InputStream inStream = conn.getInputStream();
+		FileOutputStream fs = new FileOutputStream("src/main/resources/util/ClientAppDLL.swf");
+		byte[] buffer = new byte[1024];
+		int length;
+		while ((byteread = inStream.read(buffer)) != -1) {
+			bytesum += byteread;
+			System.out.println(bytesum);
+			fs.write(buffer, 0, byteread);
+		}
+		fs.close();
+		inStream.close();
 	}
 
 	public static byte[] decompress(byte[] data) {
